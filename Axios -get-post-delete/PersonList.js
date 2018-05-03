869f2544d22195b45eb5d9144cb980d17066b918
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-05-03 16:19:55 +0200

    axios for http requests

---
+++ Axios -get-post-delete/PersonList.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+
+class PersonList extends Component {
+    state={
+        persons:[]
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/users').then(res =>{
+            console.log(res);
+            this.setState({persons:res.data});
+        })
+    }
+
+    render() {
+        return (
+            <div>
+            <ul>
+                {
+                    this.state.persons.map(person=><li key={person.id}>{person.id}-{person.name}-{person.email}</li>)
+                }
+            </ul>
+            </div>
+        );
+    }
+}
+
+export default PersonList;
