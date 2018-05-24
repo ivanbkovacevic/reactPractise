883dcc6acd0594c926f4aeac248504906ff5d552
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-05-24 19:24:52 +0200

    iy Huba

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,11 +27,14 @@ state={
 }
 
 componentDidMount(){
-    axios.get('https://react-my-burger-197af.firebaseio.com/orders/ingredients.json')
+    console.log('nesto burger');
+    axios.get('https://react-my-burger-197af.firebaseio.com/ingredients.json')
     .then(response => {
+        console.log('nesto axios');
         this.setState({ingredients:response.data});
     })
     .catch(error=>{
+        console.log('nesto burger catch');
         this.setState({error:true});
     });
 }
@@ -89,38 +92,17 @@ purchaseCancelHandler=()=>{
 }
 
 purchaseContinueHandler = ()=>{
-//     this.setState({ loading: true});
-//    // alert('You continue!');
-//    const order = {
-//        ingredients:this.state.ingredients,
-//        price: this.state.totalPrice,
-//        customer:{
-//            name:'Max Kovacevic',
-//            addres: {
-//                street: 'Tesloina 1',
-//                zipCode: '12345',
-//                country: 'Serbia'
-//            },
-//            email:'test@test.com'
-//        },
-//        deliveryMethod:'fastest'
-//    }
 
-//     axios.post('/orders.json', order)
-//     .then(response=>{
-//         this.setState({ loading : false, purchasing:false });
-//     })
-//     .catch(error=>{this.setState({loading : false, purchasing:false});
-//     });
 // 
 const queryParams= [];
 for(let i in this.state.ingredients ){
     queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 }
+queryParams.push('price='+ this.state.totalPrice);
 const queryString = queryParams.join('&');
 this.props.history.push({
     pathname: '/checkout',
-    search:'?'+queryString
+    search:'?' + queryString
 });
 }
 
@@ -132,12 +114,13 @@ this.props.history.push({
             disabledInfo[key] = disabledInfo[key] <=0
         }
         let orderSummary = null;      
-        let burger = this.state.error ? <p>Ingredients not loaded</p>: <Spinner /> ;
+        console.log(this.state.ingredients);
+        let burger = this.state.error ? <p>Ingredients not loaded</p> : <Spinner /> ;
         if(this.state.ingredients){
             burger = (
                 <Auks>
-                    <Burger ingredients={this.state.ingredients}/>
-                    <BuildControls 
+                    <Burger ingredients={this.state.ingredients}/> 
+                     <BuildControls 
                         ingredientAdded={this.addIngredientHandler} 
                         ingredientRemoved={this.removeIngredientHandler}
                         disabled={disabledInfo}
--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.css';
+import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state={
@@ -9,19 +12,63 @@ class ContactData extends Component {
         address:{
             street:'',
             postalCode:''
-        }
+        },
+        loading: false,
+     
     }
+
+    orderHandler = (event) => {
+        event.preventDefault();
+            this.setState({loading: true});
+   // alert('You continue!');
+   console.log(this.props);
+   let order = {
+       ingredients:this.props.ingredients,
+       price:  this.props.totalPrice,
+       customer:{
+           name:'Max Kovac',
+           addres: {
+               street: 'Tesloina 1',
+               zipCode: '12345',
+               country: 'Serbia',
+               grad: 'pancevo'
+           },
+           email:'test@test.com'
+       },
+       deliveryMethod:'fastest',
+       
+       
+   }
+
+    axios.post('/orders.json', order)
+    .then(response=>{
+        console.log('nesto json');
+        this.setState({ loading : false });
+        this.props.history.push('/');
+    })
+    .catch(error=>{this.setState({loading : false});
+
+    });
+    }
+
     render() {
+        let form = (     
+             <form>
+            <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+            <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
+            <Input inputtype="input" type="text" name="street" placeholder="Street" />
+            <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
+            <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+        </form>
+        );
+
+       if(this.state.loading){
+            form = <Spinner />;
+        } 
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success">ORDER</Button>
-                </form>
+                {form}
             </div>
         );
     }
